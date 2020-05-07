c5f372e5bbdba388475888686a896455e7e9fcd0
lavanyagadde <lavanyagadde217@gmail.com>
2020-05-07 11:18:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/dist/phase1")));
 
 mongoose
   .connect(config.MongoUri, { useNewUrlParser: true })
